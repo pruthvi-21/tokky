4a5f48f53120760fab22252dd3e45fdb366bde15
Pruthvi Bugidi <pruthvi.bugidi@proton.me>
2023-02-14 21:37:54 +0530

    New activity to take account details from user

--- app/src/main/java/com/ps/tokky/MainActivity.kt
+++ app/src/main/java/com/ps/tokky/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.ps.tokky
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.ps.tokky.databinding.ActivityMainBinding
+import com.ps.tokky.ui.EnterKeyDetailsActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,5 +15,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        binding.fabAddNew.setOnClickListener {
+            startActivity(Intent(this, EnterKeyDetailsActivity::class.java))
+        }
     }
 }
