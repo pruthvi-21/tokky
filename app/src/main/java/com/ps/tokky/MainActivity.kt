ad90003db8b9ee35f23d312bc288ecda12296fa4
Pruthvi Bugidi <pruthvi.bugidi@proton.me>
2023-02-14 13:12:57 +0530

    Added support for ViewBinding

--- app/src/main/java/com/ps/tokky/MainActivity.kt
+++ app/src/main/java/com/ps/tokky/MainActivity.kt
@@ -2,10 +2,16 @@ package com.ps.tokky
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.ps.tokky.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
