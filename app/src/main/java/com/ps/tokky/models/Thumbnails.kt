9ac9e7adf0da9dadaaf812d9165746b295c12766
Pruthvi Bugidi <pruthvi.bugidi@proton.me>
2023-03-10 14:41:48 +0530

    Added few brand icons Amazon, CardinalHealth, Google, Microsoft, OlympTrade, Outlook, Proton, Twitter, USF

---
+++ app/src/main/java/com/ps/tokky/models/Thumbnails.kt
@@ -0,0 +1,15 @@
+package com.ps.tokky.models
+
+import com.ps.tokky.R
+
+enum class Thumbnails(val icon: Int) {
+    Amazon(R.drawable.logo_amazon),
+    CardinalHealth(R.drawable.logo_cardinal_health),
+    Google(R.drawable.logo_google),
+    Microsoft(R.drawable.logo_microsoft),
+    OlympTrade(R.drawable.logo_olymp_trade),
+    Outlook(R.drawable.logo_outlook),
+    Proton(R.drawable.logo_proton),
+    Twitter(R.drawable.logo_twitter),
+    USF(R.drawable.logo_usf);
+}
--- app/src/main/java/com/ps/tokky/utils/TokenViewHolder.kt
+++ app/src/main/java/com/ps/tokky/utils/TokenViewHolder.kt
@@ -7,11 +7,11 @@ import android.os.Looper
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.ps.tokky.databinding.RvAuthCardBinding
+import com.ps.tokky.models.Thumbnails
 import com.ps.tokky.models.TokenEntry
 
 class TokenViewHolder(
-    val context: Context,
-    val binding: RvAuthCardBinding
+    val context: Context, val binding: RvAuthCardBinding
 ) : RecyclerView.ViewHolder(binding.root) {
 
     private var listener: Callback? = null
@@ -36,9 +36,7 @@ class TokenViewHolder(
             binding.accountLabel.visibility = View.GONE
         }
 
-        val drawable = LetterBitmap(context)
-            .getLetterTile(entry.issuer + entry.label) //appending label for different color if same issuer name
-        binding.thumbnail.setImageBitmap(drawable)
+        generateIcon()
 
         if (editModeEnabled) {
             binding.arrow.visibility = View.GONE
@@ -73,6 +71,20 @@ class TokenViewHolder(
         }
     }
 
+    private fun generateIcon() {
+        if (entry == null) return
+
+        val thumb = Thumbnails.values().find { it.name.lowercase() == entry!!.issuer.replace(" ", "").lowercase() }
+        if (thumb != null) {
+            binding.thumbnail.setImageResource(thumb.icon)
+            return
+        }
+
+        val drawable =
+            LetterBitmap(context).getLetterTile(entry?.issuer + entry?.label) //appending label for different color if same issuer name
+        binding.thumbnail.setImageBitmap(drawable)
+    }
+
     var isExpanded = false
         set(value) {
             field = value
@@ -86,10 +98,7 @@ class TokenViewHolder(
 
             binding.cardHiddenLayout.visibility = if (field) View.VISIBLE else View.GONE
 
-            binding.arrow.animate()
-                .rotation(if (value) 180f else 0f)
-                .setDuration(200)
-                .start()
+            binding.arrow.animate().rotation(if (value) 180f else 0f).setDuration(200).start()
         }
 
     var editModeEnabled = false
