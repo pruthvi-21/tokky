74ae06272ddbc8cd6d6f2d4672bf018708ad784b
Pruthvi Bugidi <pruthvi.bugidi@proton.me>
2023-09-18 15:07:09 +0530

    Disable export accounts preference when accounts list is empty

--- app/src/main/java/com/ps/tokky/preference/ExportAccountsPreference.kt
+++ app/src/main/java/com/ps/tokky/preference/ExportAccountsPreference.kt
@@ -7,11 +7,16 @@ import androidx.preference.Preference
 import androidx.preference.Preference.OnPreferenceClickListener
 import androidx.preference.PreferenceFragmentCompat
 import com.ps.tokky.activities.transfer.export.ExportActivity
+import com.ps.tokky.database.DBHelper
 
 class ExportAccountsPreference @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
 ) : Preference(context, attrs) {
+
+    init {
+        isEnabled = DBHelper.getInstance(context).getAll(false).size != 0
+    }
 
     fun setupListeners(preferenceFragment: PreferenceFragmentCompat) {
         onPreferenceClickListener = OnPreferenceClickListener {
