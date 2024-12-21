b5be6a9361d26cfb0d5d8aa43c0448e30a3ec71a
Pruthvi Bugidi <pruthvi.bugidi@proton.me>
2024-12-21 23:39:13 +0530

    feat: enable edge 2 edge in BarcodeScanningActivity

--- camerax-library/src/main/java/com/ps/camerax/BarcodeScanningActivity.kt
+++ camerax-library/src/main/java/com/ps/camerax/BarcodeScanningActivity.kt
@@ -9,6 +9,7 @@ import android.util.Size
 import android.view.OrientationEventListener
 import android.view.Surface
 import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageAnalysis
@@ -37,6 +38,7 @@ class BarcodeScanningActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(R.layout.activity_barcode_scanning)
         setTheme(R.style.FullscreenTheme)
 
