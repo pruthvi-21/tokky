b714f6f43893a01e18f5dc8e1bd00cf8cd616842
Pruthvi Bugidi <pruthvi.bugidi@proton.me>
2024-12-29 14:40:18 +0530

    style: update TokkyDialog layout

--- app/src/main/java/com/ps/tokky/ui/components/dialogs/TokkyDialog.kt
+++ app/src/main/java/com/ps/tokky/ui/components/dialogs/TokkyDialog.kt
@@ -1,21 +1,29 @@
 package com.ps.tokky.ui.components.dialogs
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
 import com.ps.tokky.R
 
@@ -30,39 +38,54 @@ fun TokkyDialog(
     onConfirmation: () -> Unit,
     content: (@Composable () -> Unit)? = null,
 ) {
-    AlertDialog(
-        modifier = Modifier
-            .fillMaxWidth(0.9f)
-            .widthIn(max = 500.dp),
+    Dialog(
         properties = DialogProperties(
             usePlatformDefaultWidth = false,
             decorFitsSystemWindows = true,
             dismissOnClickOutside = true,
             dismissOnBackPress = true
         ),
-        shape = RoundedCornerShape(16.dp),
         onDismissRequest = {
             onDismissRequest()
-        },
-        confirmButton = {
-            TextButton(onClick = { onConfirmation() }) {
-                Text(text = confirmText)
+        }
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth(0.9f)
+                .widthIn(max = 500.dp)
+                .clip(RoundedCornerShape(4.dp))
+                .background(MaterialTheme.colorScheme.surfaceContainer),
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .heightIn(min = 54.dp)
+                    .padding(horizontal = 24.dp)
+                    .padding(top = 18.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                icon?.invoke()
+                if (dialogTitle != null) {
+                    if (icon != null) Spacer(Modifier.height(5.dp))
+                    else Spacer(Modifier.height(10.dp))
+                    Text(
+                        text = dialogTitle,
+                        fontSize = 20.sp,
+                        fontWeight = FontWeight.Bold,
+                    )
+                }
             }
-        },
-        dismissButton = {
-            TextButton(onClick = { onDismissRequest() }) {
-                Text(text = dismissText)
-            }
-        },
-        icon = { icon?.invoke() },
-        title = if (dialogTitle != null) {
-            { Text(text = dialogTitle, fontSize = 18.sp) }
-        } else null,
-        text = {
-            Column {
+
+            Column(
+                modifier = Modifier.padding(horizontal = 24.dp)
+            ) {
                 if (!dialogBody.isNullOrEmpty()) {
-                    Spacer(Modifier.height(10.dp))
-                    Text(text = dialogBody)
+                    Text(
+                        text = dialogBody,
+                        fontSize = 15.sp,
+                        color = MaterialTheme.colorScheme.onSurfaceVariant,
+                        modifier = Modifier.padding(vertical = 15.dp)
+                    )
                 }
 
                 if (content != null) {
@@ -72,6 +95,28 @@ fun TokkyDialog(
                 }
             }
 
+            Row(
+                Modifier
+                    .padding(vertical = 4.dp, horizontal = 12.dp)
+                    .heightIn(min = 54.dp)
+            ) {
+                Spacer(Modifier.weight(1f))
+                TextButton(
+                    onClick = { onDismissRequest() },
+                    shape = RoundedCornerShape(4.dp),
+                    modifier = Modifier.widthIn(min = 70.dp)
+                ) {
+                    Text(text = dismissText)
+                }
+                Spacer(Modifier.width(5.dp))
+                TextButton(
+                    onClick = { onConfirmation() },
+                    shape = RoundedCornerShape(4.dp),
+                    modifier = Modifier.widthIn(min = 70.dp)
+                ) {
+                    Text(text = confirmText)
+                }
+            }
         }
-    )
+    }
 }
