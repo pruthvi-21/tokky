4258214939bf4ece45663da76dfad96ce1bf1a04
Pruthvi Bugidi <pruthvi.bugidi@proton.me>
2025-01-29 09:17:20 +0530

    refactor: improve android back animation

--- composeApp/src/androidMain/kotlin/com/boxy/authenticator/navigation/PlatformBackAnimation.android.kt
+++ composeApp/src/androidMain/kotlin/com/boxy/authenticator/navigation/PlatformBackAnimation.android.kt
@@ -6,26 +6,26 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.layout
 import androidx.compose.ui.unit.dp
-import com.arkivanov.decompose.ExperimentalDecomposeApi
 import com.arkivanov.decompose.extensions.compose.stack.animation.StackAnimation
 import com.arkivanov.decompose.extensions.compose.stack.animation.StackAnimator
 import com.arkivanov.decompose.extensions.compose.stack.animation.isFront
-import com.arkivanov.decompose.extensions.compose.stack.animation.predictiveback.predictiveBackAnimation
 import com.arkivanov.decompose.extensions.compose.stack.animation.stackAnimation
 import com.arkivanov.decompose.extensions.compose.stack.animation.stackAnimator
 import com.arkivanov.essenty.backhandler.BackHandler
 
-@OptIn(ExperimentalDecomposeApi::class)
 actual fun <C : Any, T : Any> backAnimation(
     backHandler: BackHandler,
     onBack: () -> Unit,
 ): StackAnimation<C, T> = stackAnimation(androidBackAnimation())
 
-private fun androidBackAnimation(animationSpec: FiniteAnimationSpec<Float> = tween(300)): StackAnimator =
+private fun androidBackAnimation(animationSpec: FiniteAnimationSpec<Float> = tween(250)): StackAnimator =
     stackAnimator(animationSpec = animationSpec) { factor, direction, content ->
+        val adjustedAlpha = if (factor <= 0.3f) 1f
+        else 1f - ((factor - 0.3f) * (1f / 0.7f))
+
         content(
             Modifier
-                .graphicsLayer(alpha = 1f - factor)
+                .graphicsLayer(alpha = adjustedAlpha)
                 .offsetYFactor(factor = if (direction.isFront) factor else factor * 0.5f)
         )
     }
