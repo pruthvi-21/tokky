34b8c288e37b08cb9977b061f8f597677ec3d284
Pruthvi Bugidi <pruthvi.bugidi@proton.me>
2025-02-19 00:25:31 +0530

    style: redesigned export tokens page with preferences

--- composeApp/src/commonMain/kotlin/com/boxy/authenticator/ui/screens/ExportTokensScreen.kt
+++ composeApp/src/commonMain/kotlin/com/boxy/authenticator/ui/screens/ExportTokensScreen.kt
@@ -1,19 +1,9 @@
 package com.boxy.authenticator.ui.screens
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.CheckboxDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -21,31 +11,33 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.focus.FocusDirection
-import androidx.compose.ui.platform.LocalFocusManager
-import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
 import boxy_authenticator.composeapp.generated.resources.Res
-import boxy_authenticator.composeapp.generated.resources.confirm_password
-import boxy_authenticator.composeapp.generated.resources.encrypt_backup
-import boxy_authenticator.composeapp.generated.resources.enter_password_for_backup
+import boxy_authenticator.composeapp.generated.resources.error_fetching_tokens
 import boxy_authenticator.composeapp.generated.resources.export
 import boxy_authenticator.composeapp.generated.resources.export_accounts
+import boxy_authenticator.composeapp.generated.resources.export_to_boxy_file
+import boxy_authenticator.composeapp.generated.resources.export_to_plain_text
 import boxy_authenticator.composeapp.generated.resources.i_understand_the_risk
-import boxy_authenticator.composeapp.generated.resources.password
-import boxy_authenticator.composeapp.generated.resources.reenter_above_password
-import boxy_authenticator.composeapp.generated.resources.show_password
+import boxy_authenticator.composeapp.generated.resources.warning
 import boxy_authenticator.composeapp.generated.resources.warning_backup_encryption
 import boxy_authenticator.composeapp.generated.resources.warning_no_backup_encryption
 import com.boxy.authenticator.navigation.LocalNavController
-import com.boxy.authenticator.ui.components.BoxSwitch
-import com.boxy.authenticator.ui.components.StyledTextField
-import com.boxy.authenticator.ui.components.TokkyButton
+import com.boxy.authenticator.ui.components.BoxyPreferenceScreen
 import com.boxy.authenticator.ui.components.Toolbar
+import com.boxy.authenticator.ui.components.dialogs.SetPasswordDialog
+import com.boxy.authenticator.ui.components.dialogs.TokkyDialog
 import com.boxy.authenticator.ui.viewmodels.ExportTokensViewModel
+import com.jw.preferences.Preference
+import com.jw.preferences.PreferenceCategory
 import org.jetbrains.compose.resources.stringResource
 import org.koin.compose.viewmodel.koinViewModel
 import org.koin.core.annotation.KoinExperimentalAPI
@@ -57,6 +49,10 @@ fun ExportTokensScreen() {
     val navController = LocalNavController.current
     val exportViewModel: ExportTokensViewModel = koinViewModel()
 
+    LaunchedEffect(Unit) {
+        exportViewModel.loadAllTokens()
+    }
+
     Scaffold(
         topBar = {
             Toolbar(
@@ -66,149 +62,100 @@ fun ExportTokensScreen() {
             )
         }
     ) { contentPadding ->
+
         Column(
             modifier = Modifier
                 .padding(contentPadding)
-                .padding(horizontal = 16.dp, vertical = 10.dp)
-                .verticalScroll(rememberScrollState())
+                .padding(horizontal = 16.dp)
         ) {
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
-                modifier = Modifier
-                    .clip(MaterialTheme.shapes.medium)
-                    .background(MaterialTheme.colorScheme.surfaceVariant)
-                    .clickable { exportViewModel.toggleIsEncrypted() }
-                    .padding(vertical = 8.dp, horizontal = 15.dp)
-                    .heightIn(42.dp),
-            ) {
-                Text(stringResource(Res.string.encrypt_backup))
-                Spacer(Modifier.weight(1f))
-                BoxSwitch(
-                    checked = exportViewModel.isEncrypted,
-                    onCheckedChange = { exportViewModel.toggleIsEncrypted() }
+            if (exportViewModel.tokensFetchError.value) {
+                Text(
+                    text = stringResource(Res.string.error_fetching_tokens),
+                    color = MaterialTheme.colorScheme.error,
                 )
             }
+            val exportEnabled =
+                !exportViewModel.tokensFetchError.value && exportViewModel.areTokensAvailable
 
-            AnimatedVisibility(
-                visible = !exportViewModel.isEncrypted,
-                content = {
-                    Column(
-                        verticalArrangement = Arrangement.spacedBy(10.dp),
-                        modifier = Modifier.padding(vertical = 10.dp),
+            BoxyPreferenceScreen {
+                item {
+                    PreferenceCategory(
+                        title = { },
                     ) {
-                        WarningText(stringResource(Res.string.warning_no_backup_encryption))
-                        Checkbox(
-                            checked = exportViewModel.isUnencryptedAcknowledged,
-                            toggle = {
-                                exportViewModel.toggleIsUnencryptedAcknowledged()
+                        Preference(
+                            title = { Text(stringResource(Res.string.export_to_boxy_file)) },
+                            enabled = exportEnabled,
+                            onClick = {
+                                exportViewModel.showSetPasswordDialog.value = true
                             },
-                            label = stringResource(Res.string.i_understand_the_risk),
+                        )
+                        Preference(
+                            title = { Text(stringResource(Res.string.export_to_plain_text)) },
+                            enabled = exportEnabled,
+                            onClick = {
+                                exportViewModel.showPlainTextWarningDialog.value = true
+                            },
+                            showDivider = false,
                         )
                     }
                 }
-            )
-
-            AnimatedVisibility(
-                visible = exportViewModel.isEncrypted,
-                content = {
-                    Column(
-                        verticalArrangement = Arrangement.spacedBy(10.dp),
-                        modifier = Modifier.padding(vertical = 10.dp),
-                    ) {
-                        WarningText(stringResource(Res.string.warning_backup_encryption))
-
-                        val focusManager = LocalFocusManager.current
-                        StyledTextField(
-                            value = exportViewModel.password,
-                            onValueChange = { exportViewModel.updatePassword(it) },
-                            label = stringResource(Res.string.password),
-                            placeholder = stringResource(Res.string.enter_password_for_backup),
-                            isPasswordField = !exportViewModel.showPassword,
-                            hidePasswordVisibilityEye = true,
-                            errorMessage = exportViewModel.passwordError,
-                            keyboardOptions = KeyboardOptions.Default.copy(
-                                imeAction = ImeAction.Next
-                            ),
-                            keyboardActions = KeyboardActions(
-                                onNext = { focusManager.moveFocus(FocusDirection.Down) }
-                            ),
-                        )
-                        StyledTextField(
-                            value = exportViewModel.confirmPassword,
-                            onValueChange = { exportViewModel.updateConfirmPassword(it) },
-                            label = stringResource(Res.string.confirm_password),
-                            placeholder = stringResource(Res.string.reenter_above_password),
-                            isPasswordField = !exportViewModel.showPassword,
-                            hidePasswordVisibilityEye = true,
-                            errorMessage = exportViewModel.confirmPasswordError,
-                            keyboardOptions = KeyboardOptions.Default.copy(
-                                imeAction = ImeAction.Done
-                            ),
-                            keyboardActions = KeyboardActions(
-                                onNext = { focusManager.clearFocus() }
-                            ),
-                        )
-                        Checkbox(
-                            checked = exportViewModel.showPassword,
-                            toggle = {
-                                exportViewModel.toggleShowPassword()
-                            },
-                            label = stringResource(Res.string.show_password),
-                            modifier = Modifier.align(Alignment.End),
-                        )
-                    }
-                }
-            )
-
-            Spacer(Modifier.weight(1f))
-            TokkyButton(
-                onClick = {
-                    exportViewModel.exportTokensToFile {
-                        navController.navigateUp()
-                    }
-                },
-                enabled = exportViewModel.isExportEnabled,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(vertical = 10.dp)
-                    .heightIn(min = 46.dp)
-            ) {
-                Text(stringResource(Res.string.export))
             }
         }
-    }
-}
 
-@Composable
-private fun WarningText(text: String) {
-    Text(
-        text = text,
-        color = MaterialTheme.colorScheme.error,
-    )
-}
+        if (exportViewModel.showPlainTextWarningDialog.value) {
+            var isUnencryptedAcknowledged by remember { mutableStateOf(false) }
 
-@Composable
-private fun Checkbox(
-    checked: Boolean,
-    toggle: () -> Unit,
-    label: String,
-    modifier: Modifier = Modifier,
-) {
-    Row(
-        modifier = modifier
-            .clip(MaterialTheme.shapes.small)
-            .clickable { toggle() }
-            .padding(end = 15.dp),
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        Checkbox(
-            checked = checked,
-            onCheckedChange = { toggle() },
-            colors = CheckboxDefaults.colors().copy(
-                uncheckedBorderColor = MaterialTheme.colorScheme.outline,
+            TokkyDialog(
+                dialogTitle = stringResource(Res.string.warning),
+                confirmText = stringResource(Res.string.export),
+                confirmEnabled = isUnencryptedAcknowledged,
+                onDismissRequest = {
+                    exportViewModel.showPlainTextWarningDialog.value = false
+                },
+                onConfirmation = {
+                    exportViewModel.showPlainTextWarningDialog.value = false
+                    exportViewModel.exportToPlainTextFile()
+                },
+            ) {
+                Column {
+                    Text(stringResource(Res.string.warning_no_backup_encryption))
+                    Row(
+                        modifier = Modifier
+                            .padding(vertical = 5.dp)
+                            .align(Alignment.End)
+                            .clip(MaterialTheme.shapes.small)
+                            .clickable { isUnencryptedAcknowledged = !isUnencryptedAcknowledged }
+                            .padding(end = 15.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Checkbox(
+                            checked = isUnencryptedAcknowledged,
+                            onCheckedChange = {
+                                isUnencryptedAcknowledged = !isUnencryptedAcknowledged
+                            },
+                            colors = CheckboxDefaults.colors().copy(
+                                uncheckedBorderColor = MaterialTheme.colorScheme.outline,
+                            )
+                        )
+
+                        Text(stringResource(Res.string.i_understand_the_risk))
+                    }
+                }
+            }
+        }
+
+        if (exportViewModel.showSetPasswordDialog.value) {
+            SetPasswordDialog(
+                dialogBody = stringResource(Res.string.warning_backup_encryption),
+                confirmText = stringResource(Res.string.export),
+                onDismissRequest = {
+                    exportViewModel.showSetPasswordDialog.value = false
+                },
+                onConfirmation = { password ->
+                    exportViewModel.showSetPasswordDialog.value = false
+                    exportViewModel.exportToBoxyFile(password)
+                }
             )
-        )
-
-        Text(label)
+        }
     }
 }
--- composeApp/src/commonMain/kotlin/com/boxy/authenticator/ui/viewmodels/ExportTokensViewModel.kt
+++ composeApp/src/commonMain/kotlin/com/boxy/authenticator/ui/viewmodels/ExportTokensViewModel.kt
@@ -1,12 +1,10 @@
 package com.boxy.authenticator.ui.viewmodels
 
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import boxy_authenticator.composeapp.generated.resources.Res
-import boxy_authenticator.composeapp.generated.resources.password_empty
-import boxy_authenticator.composeapp.generated.resources.password_mismatch
+import com.boxy.authenticator.core.Logger
 import com.boxy.authenticator.core.crypto.Crypto
 import com.boxy.authenticator.core.serialization.BoxyJson
 import com.boxy.authenticator.domain.models.ExportableTokenEntry
@@ -17,124 +15,59 @@ import com.boxy.authenticator.utils.Constants
 import com.boxy.authenticator.utils.Constants.EXPORT_ENCRYPTED_FILE_EXTENSION
 import com.boxy.authenticator.utils.Constants.EXPORT_FILE_EXTENSION
 import io.github.vinceglb.filekit.core.FileKit
+import io.github.vinceglb.filekit.core.PlatformFile
 import kotlinx.coroutines.launch
 import kotlinx.datetime.Clock
 import kotlinx.datetime.TimeZone
 import kotlinx.datetime.toLocalDateTime
 import kotlinx.serialization.json.JsonArray
 import kotlinx.serialization.json.encodeToJsonElement
-import org.jetbrains.compose.resources.getString
 
 class ExportTokensViewModel(
     private val fetchTokensUseCase: FetchTokensUseCase,
 ) : ViewModel() {
+    private val logger = Logger("ExportTokensViewModel")
 
-    private val _isEncrypted = mutableStateOf(true)
-    val isEncrypted by _isEncrypted
+    val showPlainTextWarningDialog = mutableStateOf(false)
+    val showSetPasswordDialog = mutableStateOf(false)
 
-    private val _isUnencryptedAcknowledged = mutableStateOf(false)
-    val isUnencryptedAcknowledged by _isUnencryptedAcknowledged
+    private var tokensList = mutableStateListOf<TokenEntry>()
+    val tokensFetchError = mutableStateOf(false)
+    val areTokensAvailable get() = tokensList.isNotEmpty()
 
-    private val _password = mutableStateOf("")
-    val password by _password
+    fun loadAllTokens() {
+        tokensFetchError.value = false
+        tokensList.clear()
 
-    private val _passwordError = mutableStateOf<String?>(null)
-    val passwordError by _passwordError
-
-    private val _confirmPassword = mutableStateOf("")
-    val confirmPassword by _confirmPassword
-
-    private val _confirmPasswordError = mutableStateOf<String?>(null)
-    val confirmPasswordError by _confirmPasswordError
-
-    private val _showPassword = mutableStateOf(false)
-    val showPassword by _showPassword
-
-    val isExportEnabled: Boolean
-        get() {
-            return if (!isEncrypted) isUnencryptedAcknowledged
-            else password.isNotEmpty() && confirmPassword.isNotEmpty()
-        }
-
-    fun toggleIsUnencryptedAcknowledged() {
-        _isUnencryptedAcknowledged.value = !isUnencryptedAcknowledged
-    }
-
-    fun toggleShowPassword() {
-        _showPassword.value = !showPassword
-    }
-
-    fun toggleIsEncrypted() {
-        _isEncrypted.value = !isEncrypted
-        if (isEncrypted) {
-            _password.value = ""
-            _passwordError.value = ""
-            _confirmPassword.value = ""
-            _confirmPasswordError.value = ""
-        } else {
-            _isUnencryptedAcknowledged.value = false
-        }
-    }
-
-    fun updatePassword(password: String) {
-        _password.value = password
-        _passwordError.value = null
-    }
-
-    fun updateConfirmPassword(password: String) {
-        _confirmPassword.value = password
-        _confirmPasswordError.value = null
-    }
-
-    private suspend fun verifyFields(): Boolean {
-        val password = _password.value
-        val confirmPassword = _confirmPassword.value
-
-        if (password.isEmpty()) {
-            _passwordError.value = getString(Res.string.password_empty)
-            return false
-        }
-        if (confirmPassword != password) {
-            _confirmPasswordError.value = getString(Res.string.password_mismatch)
-            return false
-        }
-
-        return true
-    }
-
-    fun exportTokensToFile(onFinished: (Boolean) -> Unit) = viewModelScope.launch {
-        if (isEncrypted && !verifyFields()) return@launch
-
-        fetchTokensUseCase()
-            .onSuccess { tokens ->
-                val exportData = prepareExportData(tokens)
-                val dataToExport = encryptDataIfEnabled(exportData)
-
-                val file = FileKit.saveFile(
-                    baseName = buildFileName(),
-                    extension = if (!isEncrypted) EXPORT_FILE_EXTENSION else EXPORT_ENCRYPTED_FILE_EXTENSION,
-                    bytes = dataToExport,
-                )
-                onFinished(file != null)
+        fetchTokensUseCase().fold(
+            onSuccess = { tokensList.addAll(it) },
+            onFailure = {
+                logger.e(it.message, it)
+                tokensFetchError.value = true
             }
-            .onFailure { onFinished(false) }
+        )
     }
 
-    private fun prepareExportData(tokens: List<TokenEntry>): String {
-        return if (isEncrypted) {
-            val exportData = JsonArray(tokens.map { token ->
-                BoxyJson.encodeToJsonElement(ExportableTokenEntry.fromTokenEntry(token))
-            })
-            BoxyJson.encodeToString(exportData)
-        } else tokens.joinToString("\n") { it.generateOtpAuthUrl() }
+    fun exportToPlainTextFile() = viewModelScope.launch {
+        val exportData = tokensList.joinToString("\n") { it.generateOtpAuthUrl() }
+        saveToFile(exportData.encodeToByteArray(), EXPORT_FILE_EXTENSION)
     }
 
-    private suspend fun encryptDataIfEnabled(data: String): ByteArray {
-        return if (!isEncrypted) {
-            data.encodeToByteArray()
-        } else {
-            Crypto.encrypt(password, data)
-        }
+    fun exportToBoxyFile(password: String) = viewModelScope.launch {
+        val tokensJsonArray = JsonArray(tokensList.map { token ->
+            BoxyJson.encodeToJsonElement(ExportableTokenEntry.fromTokenEntry(token))
+        })
+        val exportData = BoxyJson.encodeToString(tokensJsonArray)
+        val encryptedExportData = Crypto.encrypt(password, exportData)
+        saveToFile(encryptedExportData, EXPORT_ENCRYPTED_FILE_EXTENSION)
+    }
+
+    private suspend fun saveToFile(data: ByteArray, extension: String): PlatformFile? {
+        return FileKit.saveFile(
+            baseName = buildFileName(),
+            extension = extension,
+            bytes = data,
+        )
     }
 
     private fun buildFileName(): String {
@@ -147,9 +80,5 @@ class ExportTokensViewModel(
                 now.hour.toString().padStart(2, '0') +
                 now.minute.toString().padStart(2, '0') +
                 now.second.toString().padStart(2, '0')
-    }
-
-    companion object {
-        private const val TAG = "ExportTokensViewModel"
     }
 }
