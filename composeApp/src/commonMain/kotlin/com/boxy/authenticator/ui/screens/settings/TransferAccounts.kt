4e2c6ae2d6c45df3271b523ecccd252cd086514f
Pruthvi Bugidi <pruthvi.bugidi@proton.me>
2025-02-19 00:23:16 +0530

    chore: remove token availability check for transfer accounts setting

--- composeApp/src/commonMain/kotlin/com/boxy/authenticator/ui/screens/settings/TransferAccounts.kt
+++ composeApp/src/commonMain/kotlin/com/boxy/authenticator/ui/screens/settings/TransferAccounts.kt
@@ -4,23 +4,14 @@ import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import boxy_authenticator.composeapp.generated.resources.Res
-import boxy_authenticator.composeapp.generated.resources.enter_password_to_decrypt
-import boxy_authenticator.composeapp.generated.resources.enter_your_password
 import boxy_authenticator.composeapp.generated.resources.export_accounts
 import boxy_authenticator.composeapp.generated.resources.export_accounts_summary
 import boxy_authenticator.composeapp.generated.resources.import_accounts
 import boxy_authenticator.composeapp.generated.resources.import_accounts_summary
-import boxy_authenticator.composeapp.generated.resources.incorrect_password
-import boxy_authenticator.composeapp.generated.resources.no_tokens_to_import
 import boxy_authenticator.composeapp.generated.resources.preference_category_transfer_accounts
-import com.boxy.authenticator.domain.models.TokenEntry
-import com.boxy.authenticator.domain.usecases.FetchTokenCountUseCase
-import com.boxy.authenticator.ui.components.dialogs.RequestPasswordDialog
 import com.jw.preferences.Preference
 import com.jw.preferences.PreferenceCategory
-import org.jetbrains.compose.resources.getString
 import org.jetbrains.compose.resources.stringResource
-import org.koin.compose.koinInject
 
 @Composable
 fun TransferAccounts(
@@ -34,7 +25,6 @@ fun TransferAccounts(
         Preference(
             title = { Text(text = stringResource(Res.string.export_accounts)) },
             summary = { Text(text = stringResource(Res.string.export_accounts_summary)) },
-            enabled = areTokensAvailable() != 0L,
             onClick = { onNavigateToExport() },
         )
         Preference(
@@ -44,9 +34,4 @@ fun TransferAccounts(
             showDivider = false,
         )
     }
-}
-
-@Composable
-fun areTokensAvailable(fetchTokenCountUseCase: FetchTokenCountUseCase = koinInject()): Long {
-    return fetchTokenCountUseCase.invoke().fold(onSuccess = { it }, onFailure = { 0 })
 }
